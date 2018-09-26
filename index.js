0c436edbf5835728bc0b6eb703da5bf066ee70d4
Sung Bae <singsang2@gmail.com>
2018-09-26 04:21:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,8 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
   else{
-    cart = [];
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    price = total()
+    cart = []
+    return `Your total cost is $${price}, which will be charged to the card ${cardNumber}.`;
   }
 }
